e906276e5b3ce6da0aba98c6f8f5e1503f2cad7f
Daniel Lin <peakmountain@hotmail.com>
2021-05-12 08:12:51 +0000

    first concurrent program

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <thread>
+
+void func()
+{
+    std::cout << std::this_thread::get_id << ": Hello Concurrent World!" << std::endl;
+}
 
 int main(void)
 {
-    std::cout << "Hello World!" << std::endl;
+    std::thread th(func);
+    th.join();
+
     return 0;
 }
