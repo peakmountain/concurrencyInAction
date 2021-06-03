514d610c3ec0ebd693c2f7b2c305580367252f44
Daniel Lin <peakmountain@hotmail.com>
2021-06-03 11:42:47 +0000

    Add calculation on function duration

--- Background_future.cpp
+++ Background_future.cpp
@@ -4,6 +4,8 @@
 
 int background_task()
 {
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::cout << "background_task: sleeping for 5 seconds." << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
@@ -12,7 +14,12 @@ int background_task()
     {
         sum += i;
     }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
     std::cout << "background_task: sum = " << sum << std::endl;
+    // std::cout << "background_task: it takes " << std::chrono::duration<double, std::chrono::seconds>(stop - start).count() << " seconds." << std::endl;
+    std::cout << "background_task: it takes " << std::chrono::duration<double, std::ratio<1,1>>(stop - start).count() << " seconds." << std::endl;
 
     return sum;
 }
@@ -24,5 +31,7 @@ int main()
     std::cout << "main: sleeping for 2 seconds." << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-    std::cout << "main: result = " << result.get() << std::endl;
+    int s = result.get();
+
+    std::cout << "main: result = " << s << std::endl;
 }
